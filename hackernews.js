f0b6e9c253270d657e3cb3a717e73b86236dbd65
bjc <wcember@gmail.com>
2015-06-09 19:34:29 -0400

    client side support - untested

--- hackernews.js
+++ hackernews.js
@@ -1,58 +1,59 @@
-"use strict";
+(function(exports){
+	if (typeof XMLHttpRequest != 'function') {
+		var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	}
 
-var exports = module.exports = {};
+	function httpGet(url)
+	{
+	    var xmlHttp = new XMLHttpRequest();
+	    xmlHttp.open( "GET", url, false );
+	    xmlHttp.send( null );
+	    return JSON.parse(xmlHttp.responseText);
+	}
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	var hn = {};
 
-function httpGet(url)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false );
-    xmlHttp.send( null );
-    return JSON.parse(xmlHttp.responseText);
-}
+	hn.getItem = function(item_id) {
+		item_id = item_id.toString();
+		var url = 'https://hacker-news.firebaseio.com/v0/item/' + item_id + '.json';
+		return httpGet(url);
+	}
 
-var hn = {};
+	hn.getUser = function(username) {
+		var url = 'https://hacker-news.firebaseio.com/v0/user/' + username +
+			'.json';
+		return httpGet(url);
+	}
 
-hn.getItem = function(item_id) {
-	item_id = item_id.toString();
-	var url = 'https://hacker-news.firebaseio.com/v0/item/' + item_id + '.json';
-	return httpGet(url);
-}
+	hn.getMaxItem = function() {
+		return httpGet('https://hacker-news.firebaseio.com/v0/maxitem.json');
+	}
 
-hn.getUser = function(username) {
-	var url = 'https://hacker-news.firebaseio.com/v0/user/' + username +
-		'.json';
-	return httpGet(url);
-}
+	hn.getTopStories = function() {
+		return httpGet('https://hacker-news.firebaseio.com/v0/topstories.json');
+	}
 
-hn.getMaxItem = function() {
-	return httpGet('https://hacker-news.firebaseio.com/v0/maxitem.json');
-}
+	hn.getNewStories = function() {
+		return httpGet('https://hacker-news.firebaseio.com/v0/newstories.json');
+	}
 
-hn.getTopStories = function() {
-	return httpGet('https://hacker-news.firebaseio.com/v0/topstories.json');
-}
+	hn.getAskStories = function () {
+		return httpGet('https://hacker-news.firebaseio.com/v0/askstories.json');
+	}
 
-hn.getNewStories = function() {
-	return httpGet('https://hacker-news.firebaseio.com/v0/newstories.json');
-}
+	hn.getShowStories = function () {
+		return httpGet('https://hacker-news.firebaseio.com/v0/showstories.json');
+	}
 
-hn.getAskStories = function () {
-	return httpGet('https://hacker-news.firebaseio.com/v0/askstories.json');
-}
+	hn.getJobStories = function () {
+		return httpGet('https://hacker-news.firebaseio.com/v0/jobstories.json');
+	}
 
-hn.getShowStories = function () {
-	return httpGet('https://hacker-news.firebaseio.com/v0/showstories.json');
-}
+	hn.getUpdates = function() {
+		return httpGet('https://hacker-news.firebaseio.com/v0/updates.json');
+	}
 
-hn.getJobStories = function () {
-	return httpGet('https://hacker-news.firebaseio.com/v0/jobstories.json');
-}
+	exports.hn = hn;
 
-hn.getUpdates = function() {
-	return httpGet('https://hacker-news.firebaseio.com/v0/updates.json');
-}
-
-exports.hn = hn;
+})(typeof exports === 'undefined'? this['hackernews-api']={}: exports);
 
